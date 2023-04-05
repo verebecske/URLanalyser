bed52935e474a784dc38ffb6c39c09c9ce4fcad3
verebecske <csuha.julia@gmail.com>
2023-04-05 18:42:10 +0200

    refactor(flask/wrapper): add try-cache and error logs

--- urlanalyser/src/flask/wrapper.py
+++ urlanalyser/src/flask/wrapper.py
@@ -3,6 +3,7 @@ import base64
 from src.url_analyser import URLAnalyser
 from src.malaut import Malaut
 from src.ancestor import Ancestor
+from werkzeug.exceptions import HTTPException, BadRequest, InternalServerError
 
 
 class FlaskAppWrapper(Ancestor):
@@ -16,14 +17,15 @@ class FlaskAppWrapper(Ancestor):
         self.debug = bool(config["debug"])
         self.config = config
         self.app = Flask(__name__)
-        self.add_all_endpoints()
+        self._add_all_endpoints()
+        self._add_errors()
 
     def run(self) -> None:
         host = self.config["host"]
         port = self.config["port"]
-        self.app.run(debug=self.debug, host=host, port=port)
+        self.app.run(debug=False, host=host, port=port)
 
-    def add_all_endpoints(self):
+    def _add_all_endpoints(self):
         self.app.add_url_rule("/", "index", self.index)
         self.app.add_url_rule("/check", "check", self.check, methods=["GET", "POST"])
         self.app.add_url_rule(
@@ -36,65 +38,116 @@ class FlaskAppWrapper(Ancestor):
             "/get_infos", "get_infos", self.get_infos, methods=["POST"]
         )
 
-    def get_infos(self):
-        try:
-            self.logger.info(request.json)
-            datas = request.json
-            status = 200
-            if "url" not in datas or datas["url"] == "":
-                status = 400
-                data = {
-                    "message": "Bad request!",
-                    "Error": "Unexpected error.",
-                }
-            else:
-                result = self.analyser.collect_infos(datas["url"], datas)
-                data = {
-                    "url": datas["url"],
-                    "result": result,
-                }
-            return jsonify(data), status
-        except Exception as e:
-            self.logger.error(e)
-            return jsonify({"error": e}), 400
+    def _add_errors(self) -> None:
+        self.app.register_error_handler(400, self._handle_bad_request)
+        self.app.register_error_handler(500, self._handle_internal_server_error)
+        self.app.register_error_handler(405, self._handle_method_not_allowed)
+        self.app.register_error_handler(404, self._handle_not_found)
 
-    def index(self):
-        data = {
-            "message": f"Hallo Welt",
-            "status": 200,
-        }
-        return jsonify(data)
+    def _handle_bad_request(self, error):
+        self.logger.error(f"Handle: {error}")
+        visible_error_messages = [
+            "Missing URL",
+            "Invalid URL",
+            "Invalid Content-Type"
+        ]
+        if error.description in visible_error_messages:
+            return f"bad request - {error.description}", 400
+        return "bad request", 400
 
-    def check(self):
-        try:
-            url = self._get_url_from_get_request()
-            result = self.analyser.check(url)
-            data = {"result": result, "url": self._encode_url(url)}
-            return jsonify(data), 200
-        except Exception as e:
-            self.logger.error(e)
-            data = {"error": "Somethings went wrong...", "url": self._encode_url(url)}
-            return jsonify(data), 400
+    def _handle_internal_server_error(self, error):
+        return "internal server error", 500
 
-    def get_screenshot(self):
-        url = self._get_url_from_get_request()
-        path = self.analyser.create_screenshot(url)
-        try:
-            return send_from_directory("/src/flask/static/", path, as_attachment=True)
-        except Exception as e:
-            return jsonify({"error": str(e)}), 404
+    def _handle_method_not_allowed(self, error):
+        return "method not allowed", 405
 
-    def get_history(self):
-        url = self._get_url_from_get_request()
-        path_list = self.analyser.get_history(url)
-        return jsonify({"path": path_list})
+    def _handle_not_found(self, error):
+        return "not found", 404
 
     def _get_url_from_get_request(self) -> str:
-        url = request.args.get("url", default=b"")
+        url = request.args.get("url", default="")
+        if url == "":
+            raise BadRequest(description="Missing URL")
+        if not self.analyser.valid_url(url):
+            raise BadRequest(description="Invalid URL")
         return self._decode_url(url)
+
+    def _get_url_from_post_request(self) -> str:
+        try:
+            data = request.json
+        except:
+            raise BadRequest(description="Invalid Content-Type")
+        if "url" not in data or data["url"] == "":
+            raise BadRequest(description="Missing URL")
+        if not self.analyser.valid_url(data["url"]):
+            raise BadRequest(description="Invalid URL")
+        return data["url"]
+
 
     def _encode_url(self, url: str):
         return base64.urlsafe_b64encode(url.encode()).decode()
 
     def _decode_url(self, url: str):
         return base64.urlsafe_b64decode(url.encode()).decode()
+
+    def get_infos(self):
+        try:
+            self.logger.info(f"Get request: {request.json}")
+            data = request.json
+            url = self._get_url_from_post_request()
+            result = self.analyser.collect_infos(data["url"], data)
+            response = {
+                "url": data["url"],
+                "result": result,
+            }
+            return jsonify(response), 200
+        except BadRequest as error:
+            raise
+        except Exception as error:
+            self.logger.error(f"Error occured while gathering infos: {error}")
+            raise InternalServerError()
+
+    def index(self):
+        self.logger.info(f"Get request: {request.data}")
+        response = {"message": "Live long and prosper!"}
+        return jsonify(response), 200
+
+    def check(self):
+        try:
+            self.logger.info(f"Get request: {request.data}")
+            if request.method == "GET":
+                url = self._get_url_from_get_request()
+            else:
+                url = self._get_url_from_post_request()
+            result = self.analyser.check(url)
+            response = {"result": result, "url": self._encode_url(url)}
+            return jsonify(response), 200
+        except BadRequest as error:
+            raise
+        except Exception as error:
+            self.logger.error(f"Error occured while checking url: {error}")
+            raise InternalServerError()
+
+    def get_screenshot(self):
+        try:
+            self.logger.info(f"Get request: {request.data}")
+            url = self._get_url_from_get_request()
+            path = self.analyser.create_screenshot(url)
+            return send_from_directory("/src/flask/static/", path, as_attachment=True)
+        except BadRequest as error:
+            raise
+        except Exception as error:
+            self.logger.error(f"Error occured while creating screenshot: {error}")
+            raise InternalServerError()
+
+    def get_history(self):
+        try:
+            self.logger.info(f"Get request: {request.json}")
+            url = self._get_url_from_get_request()
+            path_list = self.analyser.get_history(url)
+            return jsonify({"result": path_list, "url": self._encode_url(url)}), 200
+        except BadRequest as error:
+            raise
+        except Exception as error:
+            self.logger.error(f"Error occured while checking url history: {error}")
+            raise InternalServerError()
