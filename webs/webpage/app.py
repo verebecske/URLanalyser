a9aa5f5bdb9f0291178d289f03f9d30be2597fc8
verebecske <csuha.julia@gmail.com>
2023-05-27 02:19:01 +0200

    fix web - step 1

--- webs/webpage/app.py
+++ webs/webpage/app.py
@@ -8,14 +8,11 @@ class FlaskWebPage:
     debug: bool
     config: dict
 
-    def __init__(self, config: dict):
+    def __init__(self, config: dict, urlanalyser):
         self.config = config
         self.app = Flask(__name__)
-        self.analyser = (
-            f'http://{self.config["analyser_host"]}:{self.config["analyser_port"]}'
-        )
         self._add_all_endpoints()
-        # self._add_errors()
+        self.urlanalyser = urlanalyser
 
     def run(self) -> None:
         host = self.config["host"]
@@ -25,12 +22,6 @@ class FlaskWebPage:
     def _add_all_endpoints(self):
         self.app.add_url_rule("/", "index", self.index, methods=["GET", "POST"])
 
-    # def _add_errors(self) -> None:
-    #     self.app.register_error_handler(400, self._handle_bad_request)
-    #     self.app.register_error_handler(500, self._handle_internal_server_error)
-    #     self.app.register_error_handler(405, self._handle_method_not_allowed)
-    #     self.app.register_error_handler(404, self._handle_not_found)
-
     def index(self):
         if request.method == "POST":
             return self.post_index()
@@ -38,26 +29,6 @@ class FlaskWebPage:
 
     def get_index(self):
         return render_template("home.html")
-
-    def _encode_url(self, url: str):
-        return base64.urlsafe_b64encode(url.encode()).decode()
-
-    def _decode_url(self, url: str):
-        return base64.urlsafe_b64decode(url.encode()).decode()
-
-    def ask_urlanalyserapi(self, settings: dict) -> dict:
-        response = requests.post(f"{self.analyser}/get_info", json=settings)
-        if response.status_code == 200:
-            return response.json()["result"]
-        return {"result": ""}
-
-    def get_screenshot(self, url: str) -> str:
-        url = self._encode_url(url)
-        response = requests.get(f"{self.analyser}/get_screenshot?url={url}")
-        path = "./static/screenshot.png"
-        with open(path, "wb") as image_file:
-            image_file.write(response.content)
-        return "screenshot.png"
 
     def get_settings_from_form(self, request):
         settings = {
@@ -67,6 +38,9 @@ class FlaskWebPage:
             "location": request.form.get("location", False),
             "screenshot": request.form.get("screenshot", False),
             "redirection": request.form.get("redirection", False),
+            "red_all": request.form.get("red_all", False),
+            "domain_age": request.form.get("domain_age", False),
+            "domain_reputation": request.form.get("domain_reputation", False),
         }
         return settings
 
@@ -74,13 +48,90 @@ class FlaskWebPage:
         settings = self.get_settings_from_form(request)
         if settings["url"] == "":
             return render_template("home.html", error="Missing URL")
-        result = self.ask_urlanalyserapi(settings)
+        result = self.urlanalyser.ask_urlanalyserapi(settings)
         filename = ""
+        is_malicious = self.urlanalyser.check_url(settings["url"])
         if settings["screenshot"]:
             filename = self.get_screenshot(settings["url"])
         return render_template(
             "return.html", url=settings["url"], result=result, filename=filename
         )
+
+
+class URLAnalyserInterface:
+    def __init__(self, config: dict):
+        self.config = config
+        self.urlanalyser_url = (
+            f'http://{self.config["analyser_host"]}:{self.config["analyser_port"]}'
+        )
+
+    def ask_urlanalyserapi(self, settings: dict) -> dict:
+        result = {}
+        url = settings["url"]
+        result.update(self.get_info(settings))
+        if settings["redirection"]:
+            result["redirection"] = self._redirection_handler(url, settings["red_all"])
+        if settings["domain_age"]:
+            result["domain_age"] = self.domain_age_handler(url)
+        if settings["domain_reputation"]:
+            result["domain_reputation"] = self.domain_reputation_handler(url)
+        return result
+
+    def get_info(self, settings: dict) -> dict:
+        response = requests.post(f"{self.urlanalyser_url}/get_info", json=settings)
+        if response.status_code == 200:
+            return response.json()["result"]
+        return {"result": f"Server error happened: {response.text}"}
+
+    def check_url(self, url: str) -> bool:
+        try:
+            result = self.send_get_request("check", url)
+            return result.json()["result"]["is_malicious"]
+        except Exception as error:
+            pass
+        return False
+
+    def domain_age_handler(self, url):
+        return self.send_get_request("get_domain_age", url)
+
+    def domain_reputation_handler(self, url):
+        return self.send_get_request("get_domain_reputation", url)
+
+    def _redirection_handler(self, url, content):
+        if content:
+            extras = {"all": "True"}
+        else:
+            extras = None
+        return self.send_get_request("get_redirection", url, extras)
+
+    def _location_handler(self, urls, channel):
+        return self.send_get_request("get_location", url)
+
+    def send_get_request(self, endpoint, url, extras: dict = None):
+        if extras is None:
+            _extras = ""
+        else:
+            _extras = "".join([f"&{key}={value}" for key, value in extras.items()])
+        url = self._encode_url(url)
+        response = requests.get(f"{self.urlanalyser_url}/{endpoint}?url={url}{_extras}")
+        if response.status_code == 200:
+            return response.json()["result"]
+        else:
+            return {"result": f"Server error happened: {response.text}"}
+
+    def _encode_url(self, url: str):
+        return base64.urlsafe_b64encode(url.encode()).decode()
+
+    def _decode_url(self, url: str):
+        return base64.urlsafe_b64decode(url.encode()).decode()
+
+    def get_screenshot(self, url: str) -> str:
+        url = self._encode_url(url)
+        response = requests.get(f"{self.urlanalyser_url}/get_screenshot?url={url}")
+        path = "./static/screenshot.png"
+        with open(path, "wb") as image_file:
+            image_file.write(response.content)
+        return "screenshot.png"
 
 
 if __name__ == "__main__":
@@ -90,5 +141,6 @@ if __name__ == "__main__":
         "analyser_host": "urlanalyser-main",
         "analyser_port": os.getenv("URLANALYSER_PORT"),
     }
-    flaskapp = FlaskWebPage(config)
+    urlanalyser = URLAnalyserInterface(config)
+    flaskapp = FlaskWebPage(config, urlanalyser)
     flaskapp.run()
