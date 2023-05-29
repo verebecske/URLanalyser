7a57ab29cc5dfbb7dd6ca28620ac898c8e7efdab
verebecske <csuha.julia@gmail.com>
2023-05-29 02:03:52 +0200

    update config

--- urlanalyser/main.py
+++ urlanalyser/main.py
@@ -29,11 +29,18 @@ class Application(Ancestor):
     def get_config(self) -> None:
         self.config = ConfigParser()
         self.config.read("secrets/config.ini")
-        self.debug = self.config["urlanalyser"].getboolean("debug")
-        self.config["urlanalyser"].setdefault("update_delay", "300")
+
+        self.config["urlanalyser"]["debug"] = os.getenv("DEBUG", True)
+        self.config["urlanalyser"]["update_delay"] = os.getenv("UPDATE_DELAY", "300")
         self.config["urlanalyser"]["redis_host"] = os.getenv("REDIS_HOST", None)
         self.config["urlanalyser"]["selenium_host"] = os.getenv("SELENIUM_HOST", "selenium-hub")
         self.config["urlanalyser"]["selenium_post"] = os.getenv("SELENIUM_PORT", "4444")
+        self.debug = self.config["urlanalyser"]
+
+        self.config["flask"]["debug"] = os.getenv("DEBUG", True)
+        self.config["flask"]["host"] = os.getenv("FLASK_HORT", "0.0.0.0")
+        self.config["flask"]["port"] = os.getenv("FLASK_PORT", "5000")
+
 
     def start_flask(self, analyser) -> None:
         flaskwrapper = FlaskWrapper(config=self.config["flask"], analyser=analyser)
@@ -66,7 +73,9 @@ class Application(Ancestor):
             collector=self.collector,
             sample_analyser=sample_analyser,
         )
-        thread = Thread(target=self.update_static_databases, args=(config["update_delay"],))
+        thread = Thread(
+            target=self.update_static_databases, args=(config["update_delay"],)
+        )
         thread.start()
         self.start_flask(analyser)
         thread.stop()
